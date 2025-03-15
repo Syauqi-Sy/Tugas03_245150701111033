7bf2024b68939453affab88f37ead265a820ce5d
Syauqi-Sy <syauqi.sy926@gmail.com>
2025-03-15 22:20:10 +0700

    asdf

---
+++ ekspedisi.java
@@ -0,0 +1,73 @@
+class PengirimanBarang {
+    String namaPengirim;
+    String alamatTujuan;
+    double berat;
+    double biayaDasar;
+    int jarakPaket;
+    PengirimanBarang() {
+        namaPengirim = "";
+        alamatTujuan = "";
+        berat = 0;
+        biayaDasar = 0;
+        jarakPaket = 0;
+    }
+    PengirimanBarang(String sender, String toAdd, double weight, double basePrice) {
+        namaPengirim = sender;
+        alamatTujuan = toAdd;
+        berat = weight;
+        biayaDasar = basePrice;
+    }
+    void hitungOngkir(double diskonPersen) {
+        biayaDasar -= (biayaDasar * diskonPersen / 100);
+    }
+    void hitungOngkir(double diskonPersen, double biayaTambahan) {
+        biayaDasar = (biayaDasar - (biayaDasar * diskonPersen / 100)) + biayaTambahan;
+    }
+    void hitungOngkir(int jarak) {
+        double surcharge;
+        double disc;
+        jarakPaket = jarak;
+        System.out.println("Jarak pengiriman               : " + jarakPaket + " km");
+        if (jarak > 50) {
+            surcharge = biayaDasar * 10/100;
+            biayaDasar += surcharge;
+            System.out.println("Biaya tambahan (jarak > 50 km) : Rp" + surcharge);
+        } else if (jarak <= 0) {
+            System.out.println("Error: Jarak tidak boleh sama dengan atau kurang dari 0.");
+        } else {
+            disc = biayaDasar * 5/100;
+            biayaDasar -= disc;
+            System.out.println("Diskon (jarak <= 50 km)        : Rp" + disc);
+        }
+        System.out.println("Biaya pengiriman akhir         : Rp" + biayaDasar);
+    }
+
+    void displayInfoLogistik() {
+        System.out.println("---Status paket xX_BoltExpress_Xx---");
+        System.out.println("Nama pengirim                  : " + namaPengirim);
+        System.out.println("Alamat tujuan                  : " + alamatTujuan);
+        System.out.println("Berat paket                    : " + berat + " kg");
+        System.out.println("Biaya ongkir dasar             : Rp" + biayaDasar);
+    }
+}
+
+public class ekspedisi {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nama pengirim           : ");
+        String pengirim = input.next();
+        System.out.print("Masukkan alamat tujuan           : ");
+        String alamat = input.next();
+        System.out.print("Masukkan berat paket             : ");
+        double berat = input.nextDouble();
+        System.out.print("Masukkan biaya pengiriman        : ");
+        double biaya = input.nextDouble();
+        System.out.print("Masukkan jarak pengiriman        : ");
+        int jarak = input.nextInt();
+        System.out.println("");
+        input.close();
+        PengirimanBarang paket1 = new PengirimanBarang(pengirim, alamat, berat, biaya);
+        paket1.displayInfoLogistik();
+        paket1.hitungOngkir(jarak);
+    }
+}
